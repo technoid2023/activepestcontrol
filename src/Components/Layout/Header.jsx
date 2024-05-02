712e8af23b1b49c4c023f5ad1bb91e0e8d072b60
technoid2023 <136116887+technoid2023@users.noreply.github.com>
2024-05-02 13:47:12 +0530

    Update Header.jsx

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -1,50 +1,39 @@
-
 import { Link } from "react-router-dom";
-
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import logo from '../../Assests/logo.png';
 
 const Header = () => {
-
- 
- 
- 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary shadow navbar-dark bg-dark">
-
+      <nav className="navbar navbar-expand-lg bg-body-tertiary shadow navbar-dark bg-dark" style={{ position: 'fixed', top: 0, width: '100%', zIndex: 1000 }}>
         <div className="container-fluid">
-  <div style={{ display: 'flex', alignItems: 'center' }}>
-  <img src={logo} alt="logo" className="me-2" style={{ width: '3rem', height: '3rem' }} />
-  <Link className="navbar-brand" to="/" >APCS</Link>
-</div>
-
-
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <img src={logo} alt="logo" className="me-2" style={{ width: '3rem', height: '3rem' }} />
+            <Link className="navbar-brand" to="/" >APCS</Link>
+          </div>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"><FontAwesomeIcon icon={faBars}/></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-             <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
               </li>
               <li className="nav-item">
-           <Link className="nav-link active" aria-current="page" to="/about-us">About Us</Link>
+                <Link className="nav-link active" aria-current="page" to="/about-us">About Us</Link>
               </li>
               <li className="nav-item">
-           <Link className="nav-link active" aria-current="page" to="/services">Services</Link>
+                <Link className="nav-link active" aria-current="page" to="/services">Services</Link>
               </li>
               <li className="nav-item">
-           <Link className="nav-link active" aria-current="page" to="/contact-us">Contact Us</Link>
+                <Link className="nav-link active" aria-current="page" to="/contact-us">Contact Us</Link>
               </li>
-
-             
             </ul>
-            
           </div>
         </div>
       </nav>
+      <div style={{ height: '64px' }}></div> 
     </div>
   )
 }
