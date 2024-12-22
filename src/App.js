d4c5625a8353695c951e22f9c1b7a569ac128eaa
technoid2023 <136116887+technoid2023@users.noreply.github.com>
2024-12-22 20:31:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     // Wait for the component to mount, then render the hidden clue
     const timeout = setTimeout(() => {
       setClueVisible(true); // Reveal the clue after some delay
-    }, 60000); // Adjust the timeout as per need
+    }, 7200000); // Adjust the timeout as per need
 
     return () => clearTimeout(timeout); // Cleanup timeout on component unmount
   }, []);
@@ -27,10 +27,10 @@ function App() {
       </div>
 
       <p className="instructions">
-        Use <b>Developer Tools (F12)</b> to change the CSS properties of the <code>hidden-location</code> class and uncover the hidden message.
+        Use <b>Developer Tools </b> to change the CSS properties of the <code>hidden-location</code> class and uncover the hidden message.
       </p>
            <p className="instructions">
-        If failed to solve wait for 1 hour the clue will automatically pop up here
+        If failed to solve wait for <b>2</b> hour the clue will automatically pop up here
       </p>
     </div>
   );
